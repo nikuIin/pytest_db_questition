72de70b12f9a44327cf1073d77c73949d8128356
van <vexyzy@yandex.ru>
2025-07-02 21:39:58 +0400

    fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,44 +6,19 @@ from postgres_helper import DatabaseHelper
 
 # Tests with db_helper scope=func
 
-
-@async_fixture(scope="function")
-async def db_helper():
-    """Fixture to set up and tear down the test database."""
-    assert app_settings.app_mode == ModeEnum.TEST
-    assert "test" in db_settings.db_name
-
-    db_helper = DatabaseHelper(url=db_settings.db_url, echo=False)
-
-    # Create schemas and tables before tests
-    await db_helper.clear_data()
-    await db_helper.create_tables()
-    await db_helper.insert_base_data(TEST_STATEMENTS)
-
-    yield db_helper
-
-    # Clean up the database after each test
-    await db_helper.clear_data()
-
-
-@async_fixture(scope="function")
-async def async_session(db_helper):
-    """Fixture to provide a database session for each test."""
-    async with db_helper.session_factory() as session:
-        yield session
-        await session.rollback()
-
-
-# Tests with db_helper scope=session
-
 #
-# @async_fixture(scope="session")
+# @async_fixture(scope="function")
 # async def db_helper():
 #     """Fixture to set up and tear down the test database."""
 #     assert app_settings.app_mode == ModeEnum.TEST
 #     assert "test" in db_settings.db_name
 #
 #     db_helper = DatabaseHelper(url=db_settings.db_url, echo=False)
+#
+#     # Create schemas and tables before tests
+#     await db_helper.clear_data()
+#     await db_helper.create_tables()
+#     await db_helper.insert_base_data(TEST_STATEMENTS)
 #
 #     yield db_helper
 #
@@ -53,13 +28,38 @@ async def async_session(db_helper):
 #
 # @async_fixture(scope="function")
 # async def async_session(db_helper):
-#     # Create schemas and tables before tests
-#     await db_helper.clear_data()
-#     await db_helper.create_tables()
-#     await db_helper.insert_base_data(TEST_STATEMENTS)
-#
 #     """Fixture to provide a database session for each test."""
 #     async with db_helper.session_factory() as session:
 #         yield session
-#
-#     await session.rollback()
+#         await session.rollback()
+
+
+# Tests with db_helper scope=session
+
+
+@async_fixture(scope="session")
+async def db_helper():
+    """Fixture to set up and tear down the test database."""
+    assert app_settings.app_mode == ModeEnum.TEST
+    assert "test" in db_settings.db_name
+
+    db_helper = DatabaseHelper(url=db_settings.db_url, echo=False)
+
+    yield db_helper
+
+    # Clean up the database after each test
+    await db_helper.clear_data()
+
+
+@async_fixture(scope="function")
+async def async_session(db_helper):
+    # Create schemas and tables before tests
+    await db_helper.clear_data()
+    await db_helper.create_tables()
+    await db_helper.insert_base_data(TEST_STATEMENTS)
+
+    """Fixture to provide a database session for each test."""
+    async with db_helper.session_factory() as session:
+        yield session
+
+    await session.rollback()
